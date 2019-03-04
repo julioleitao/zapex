e21a3d6a34c4689c5a83db6b4f0e05163adf7eba
Júlio Leitão <contatojuliolmj@gmail.com>
2019-03-04 15:23:01 -0300

    [tool] Setup the Docker configuration
    This commit provides to develop all services inside containers or
    just use the database. The dev configuration checks if there is an
    environment variable for the database URL. The containers are running
    and share the ports with the host. It is possible to use the mix
    phx.server at the host machine or at the container. That is the reason
    why there are two different ports. Port 8080 for the host access the
    container and 4000 to access the service up at the host (default).

--- config/test.exs
+++ config/test.exs
@@ -1,18 +1,11 @@
 use Mix.Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :zapex, ZapexWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
 config :zapex, Zapex.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "zapex_test",
-  hostname: "localhost",
+  url: System.get_env("DATABASE_URL") || "postgresql://zapex_usr:zapex_pwd@localhost/zapex_test",
   pool: Ecto.Adapters.SQL.Sandbox
